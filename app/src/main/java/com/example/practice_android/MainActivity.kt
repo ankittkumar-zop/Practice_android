a1a3aa9f7b5c661d948c099cf8fcd57bc7384b00
ankittkumar-zop <ankit.kr@zopsmart.com>
2024-07-05 15:24:04 +0530

    #2 added functionality to the button

--- app/src/main/java/com/example/practice_android/MainActivity.kt
+++ app/src/main/java/com/example/practice_android/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.practice_android
 
 import android.os.Bundle
-import android.widget.Toolbar
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
+data class UserLoginInfo(val userId: String , val password:String)
 
 class MainActivity : AppCompatActivity() {
+
+    private val userLoginInfoList = mutableListOf<UserLoginInfo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,5 +21,25 @@ class MainActivity : AppCompatActivity() {
 ////        setSupportActionBar(toolbar)
 //        supportActionBar?.setDisplayShowTitleEnabled(true)
 //        supportActionBar?.title= "Welcome"
+
+        val usernameEditText : EditText = findViewById(R.id.loginId)
+        val passwordEditText : EditText = findViewById(R.id.passwordId)
+        val loginButton: Button = findViewById(R.id.LoginButton)
+
+        loginButton.setOnClickListener{
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if ( username.isNotBlank() && password.isNotBlank() ){
+                val user = UserLoginInfo(username, password)
+                userLoginInfoList.add(user)
+
+                usernameEditText.text.clear()
+                passwordEditText.text.clear()
+            }
+            else {
+                Toast.makeText(this, "Enter details", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
